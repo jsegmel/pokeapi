e05db3080f9ab30c9ac2f7bff7cf4eeaf20c5e9d
jose antonio <jsegmel@gmail.com>
2022-12-13 17:48:51 +0100

    se añaden form y foto

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-let url='https://pokeapi.co/api/v2/pokemon?limit=20'
+let url='https://pokeapi.co/api/v2/pokemon/3333'
 fetch(url)
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => mostrarPokemon(data) )
+.then(error=>console.log(error))
 
 
-const mostrarData= data.map( (pokemon)=>{
-  return pokemon
-})
-
-
-console.log(mostrarData[5])
-
+const mostrarPokemon=(data)=>{
+    console.log(data.name)
+}
